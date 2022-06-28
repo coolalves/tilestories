dc1058e2ae2fbe97eaaa5f0929c9a670fb3414c9
coolalves <alvesdaniel@ua.pt>
2022-06-28 16:59:47 +0100

    estrutura app

---
+++ src/components/HomeButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+export default function HomeButton({size, color}){
+    return(
+        <View style={styles.container}>
+            <Image source={require('../../assets/imgs/tilebutton.png')} size={size} color={color} />
+                 
+             
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: 60,
+        height:60,
+        borderRadius:0,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 32
+    }
+})
---
+++ src/pages/scoreboard/Scoreboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from 'react-native';
+
+
+export default function Scoreboard() {
+  return (
+    <Text>
+      "scoreboard"
+    </Text>
+  );
+}
