8826951731f983228dca2e7bd597fef9aa9ff43d
coolalves <alvesdaniel@ua.pt>
2022-07-01 00:34:58 +0100

    stack e camera com alguns bugs

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function HomeButton({size, color}){
-    return(
+export default function HomeButton({ size, color }) {
+    return (
         <View style={styles.container}>
             <Image source={require('../../assets/imgs/tilebutton.png')} size={size} color={color} />
-                 
-             
+
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         width: 60,
-        height:60,
-        borderRadius:0,
+        height: 60,
+        borderRadius: 0,
         backgroundColor: 'transparent',
         alignItems: 'center',
         justifyContent: 'center',
