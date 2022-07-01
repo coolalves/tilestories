3d09b73ec3642fb79953967be59cd73c62da38ce
coolalves <alvesdaniel@ua.pt>
2022-07-01 18:59:26 +0100

    tinha uma route errada

--- src/routes.js
+++ src/routes.js
@@ -73,7 +73,7 @@ export default function Routes() {
                     )
                 }}
             />
-            <Tab.Screen name="Account" component={CameraScreen}
+            <Tab.Screen name="Account" component={Profile}
                 options={{
                     tabBarLabel: '',
                     tabBarIcon: ({ size, color }) => (
