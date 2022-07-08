08657b210490d377782fde48f7a8ef5a8e8cac37
Daniel Alves <alvesdaniel@ua.pt>
2022-07-09 00:36:39 +0100

    landing page funcional

--- src/pages/login/Landing.js
+++ src/pages/login/Landing.js
@@ -7,73 +7,8 @@ import { doc, setDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import { AuthContext } from '../../context';
 
-const Landing = () => {
+const Landing = ({ navigation: { navigate } }) => {
 
-    const [email, setEmail]= React.useState('');
-    const [password, setPassword]= React.useState('');
-    var user;
-
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const db= getFirestore(app);
-
-    const handleCreateAccount = () => {
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential)=>{
-                console.log('Account Createddd!!');
-                user = userCredential.user;
-
-                const myDoc = doc(db, "users", user.uid)
-
-                // Your Document Goes Here
-                const docData = {
-                    "email": user.email ,
-                    "name": "nomealia",
-                    "uid": user.uid,
-                    "resgisterDate": user.metadata.creationTime
-                }
-
-                setDoc(myDoc, docData)
-                    // Handling Promises
-                    .then(() => {
-                        // MARK: Success
-                        console.log('vai para o firebase!!');
-                    })
-                    .catch((error) => {
-                        // MARK: Failure
-                        alert(error.message)
-                    })
-
-
-            })
-            .catch(error => {
-                console.log(error);
-            })
-
-        //passar para a base de dados
-
-
-
-        // MARK: Creating New Doc in Firebase
-        // Before that enable Firebase in Firebase Console
-
-
-
-
-    }
-
-    const handleSignIn = () => {
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential)=>{
-                console.log('Signed In!');
-                const user = userCredential.user;
-                console.log(user);
-
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
 
 
 
@@ -92,14 +27,14 @@ const Landing = () => {
             </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity
-                    onPress={handleSignIn}
-                    style={[styles.buttonText,styles.buttonOutlinelog]}
+                    onPress={() => navigate('Login')}
+                    style={[styles.buttonText, styles.buttonOutlinelog]}
                 >
                     <Text style={styles.textbranco}>Login</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                    onPress={handleCreateAccount}
+                    onPress={() => navigate('Signup')}
                     style={[styles.button, styles.buttonOutline]}>
                     <Text>Register</Text>
                 </TouchableOpacity>
@@ -117,13 +52,13 @@ const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
         justifyContent: 'center',
-        flex:3
+        flex: 3
     },
-    inputContainer:{
+    inputContainer: {
         width: '80%'
     },
-    input:{
-        backgroundColor:'white',
+    input: {
+        backgroundColor: 'white',
         paddingHorizontal: 15,
         paddingVertical: 10,
         marginTop: 5,
@@ -135,12 +70,12 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-        borderRadius:10,
-        marginVertical:10,
+        borderRadius: 10,
+        marginVertical: 10,
 
     },
 
-    buttonContainer:{
+    buttonContainer: {
         width: '50%',
         justifyContent: 'center',
         alignItems: 'center',
@@ -148,45 +83,45 @@ const styles = StyleSheet.create({
 
     },
 
-    button:{
-        backgroundColor:'#0782F9',
-        width:'100%',
-        padding:15,
-        borderRadius:10,
-        alignItems:"center",
+    button: {
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
     },
-    buttonOutline:{
+    buttonOutline: {
         backgroundColor: 'white',
         marginTop: 5,
         borderColor: '#5C75DD',
-        borderWidth:2,
+        borderWidth: 2,
     },
 
-    buttonOutlinelog:{
+    buttonOutlinelog: {
         backgroundColor: '#5C75DD',
         marginTop: 5,
         marginBottom: 8,
         borderColor: '#5C75DD',
-        borderWidth:2,
+        borderWidth: 2,
 
     },
-    buttonText:{
-        backgroundColor:'#5C75DD',
-        width:'100%',
-        padding:15,
-        borderRadius:10,
-        alignItems:"center",
+    buttonText: {
+        backgroundColor: '#5C75DD',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
     },
-    buttonOutlineText:{
-        color:'#0782F9',
+    buttonOutlineText: {
+        color: '#0782F9',
         fontWeight: '700',
-        fontSize:16,
+        fontSize: 16,
     },
-    fotinha:{
-        marginBottom:80,
+    fotinha: {
+        marginBottom: 80,
     },
-    textbranco:{
-        color:'white',
+    textbranco: {
+        color: 'white',
         fontWeight: '500'
     },
 
